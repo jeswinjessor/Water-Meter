5f4f59d5c40105f0aff705f6e6ce59246d34005d
Jeswin J <jeswin.jessor@gmail.com>
2021-05-17 20:31:08 -0400

    Add project files.

---
+++ TakeHomeMidterm/HistoryWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TakeHomeMidterm
+{
+    /// <summary>
+    /// Interaction logic for HistoryWindow.xaml
+    /// </summary>
+    public partial class HistoryWindow : Window 
+    {
+        //MainWindow MainWindow = new MainWindow();
+
+       
+        public HistoryWindow()
+        {
+            InitializeComponent();
+            Dictionary<double, double> _HistoryListView = MainWindow._historyDictory;
+            List<Items> list = new List<Items>();
+            foreach (KeyValuePair<double, double> HistoryList in _HistoryListView)
+            {
+                list.Add(new Items(HistoryList.Key, HistoryList.Value));
+            }
+            var query = (from d in list orderby d.GallonsUsed select new { d.GallonsUsed, d.TotalCharge });
+            HistoryItems.ItemsSource = query.ToList();
+            
+        }
+
+
+
+
+    }
+
+
+    public class Items
+    {
+        public double GallonsUsed { get; set; }
+        public double TotalCharge { get; set; }
+
+        public Items(double GallonsUsed, double TotalCharge)
+        {
+            this.GallonsUsed = GallonsUsed;
+            this.TotalCharge = TotalCharge;
+        }
+    }
+}
---
+++ TakeHomeMidterm/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TakeHomeMidterm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       public static Dictionary<double, double> _historyDictory = new Dictionary<double, double>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtCurrentReading.Focus();
+            
+        }
+
+
+        private void OnClickCalculate(object sender, RoutedEventArgs e) {
+            double ChargeForWater = 0.00175;
+            double CurrentReading = Convert.ToDouble(txtCurrentReading.Text);
+            double PreviousReading = Convert.ToDouble(txtPreviousReading.Text);
+
+            if (CurrentReading >= PreviousReading)
+            {
+                double GallonsUsed = CurrentReading - PreviousReading;
+                double TotalCharge = GallonsUsed * ChargeForWater;
+                txtGallonsUsed.Text = Convert.ToString(GallonsUsed);
+                txtTotalCharge.Text = Convert.ToString(TotalCharge);
+                try
+                {
+                    _historyDictory[GallonsUsed] = TotalCharge;
+                    foreach (var s in _historyDictory)
+                    {
+                        txtErrorMessage.Text = Convert.ToString(s.Key);
+                    }
+                }
+                catch 
+                {
+                    txtErrorMessage.Text = "This Reading is already Added!";
+                }
+            }
+            else
+            {
+                txtErrorMessage.Text = "Current Reading is less than Previous Reading!!";
+            }
+
+            
+        }
+        private void About(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Water Meter");
+        }
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void OnClickHistory(object sender, RoutedEventArgs e) 
+        {
+             HistoryWindow History = new HistoryWindow();
+             History.ShowDialog();
+
+
+        }
+
+       
+    }
+
+
+}
